27975088a8371dcc106d36dc5f628978767f24f9
Shahrukh <shahrukh1494@gmail.com>
2024-06-19 18:29:31 +0530

    aliases and re-tryablility

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,11 +4,27 @@ describe("form tests", () => {
   });
   it("test subscribe form", () => {
     cy.contains(/testing forms/i);
-    cy.getDataTest("subscribe-form").find("input").type("shahrukh@elred.com");
+    cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
+    cy.get("@subscribe-input").type("shahrukh@elred.com");
     cy.contains(/successfully subbed: shahrukh@elred.com!/i).should(
       "not.exist"
     );
     cy.getDataTest("subscribe-button").click();
     cy.contains(/successfully subbed: shahrukh@elred.com!/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/successfully subbed: shahrukh@elred.com!/i).should(
+      "not.exist"
+    );
+
+    cy.get("@subscribe-input").type("shahrukh@elred.in");
+    cy.contains(/invalid email: shahrukh@elred.in!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/invalid email: shahrukh@elred.in!/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/invalid email: shahrukh@elred.in!/i).should("not.exist");
+
+    cy.contains(/fail!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/fail!/i).should("exist");
   });
 });
