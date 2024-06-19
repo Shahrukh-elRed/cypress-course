01e8ddcfe26a8272e99454b5aaf731aea7d45c2b
Shahrukh <shahrukh1494@gmail.com>
2024-06-19 19:13:30 +0530

    Multi-Page Testing

---
+++ cypress/e2e/examples.cy.js
@@ -0,0 +1,18 @@
+describe("various examples", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("multi-page testing", () => {
+    cy.getDataTest("nav-why-cypress").click();
+    cy.location("pathname").should("equal", "/");
+
+    cy.getDataTest("nav-overview").click();
+    cy.location("pathname").should("equal", "/overview");
+
+    cy.getDataTest("nav-fundamentals").click();
+    cy.location("pathname").should("equal", "/fundamentals");
+
+    cy.getDataTest("nav-examples").click();
+    cy.location("pathname").should("equal", "/examples");
+  });
+});
